b4aee1bf5daf35b2dda83ef7d73464ab6ebde214
phamtienedvin <114471794+phamtienedvin@users.noreply.github.com>
2025-04-08 12:31:11 +0200

    Add files via upload

--- python2.py
+++ python2.py
@@ -1,118 +1,12 @@
 #--------------------------
-'''
-    A kereses_a_listaban nevű függvény.
-    Első bemeneti paraméter egy lista,
-    második bemeneti paraméter egy szám.
-    A visszatérési érték a paraméterként megadott szám
-        első előfordulási helye a listában.
-    
-    A visszatérési érték None, ha a szám nics benne a listában.
-'''
-
-def kereses_a_listaban(lista, szam):
-    index = 0
-    for x in lista:
-        if(szam == x):
-            return index
-        else:
-            index += 1
-
-    return None
-
-
-#--------------------------
 ''' 
-    Az elso_karakter nevű függvény,
-    paraméterként egy stringet kap és
-    visszatér a string első karakterével.
-    
-    Üres string esetén None a visszatérési érték.
-'''
-
-
-
-#--------------------------
-''' 
-    Az osszeg  nevű függvény,
+    A pozitivok_szama nevű függvény,
     paraméterként egy számokat tartalmazó listát kap és
-    visszatér a lista számainak összegével.
-    Üres lista esetén 0 a visszatérési érték.
-    A feladat megoldása során nem használhatod a sum() függvényt!
-'''
-
-
-
-#--------------------------
-''' 
-    A pozitivok_kivalogatasa nevű függvény,
-    visszatér egy listával, 
-        amely a paraméterként átadott számokat tartalmazó lista
-        pozitiv számait tartalmazza. 
-'''
-
-
-
-#--------------------------
-''' 
-    A szorzat  nevű függvény,
-    paraméterként egy számokat tartalmazó listát kap és
-    visszatér a lista számainak szorzatával.
-    
-    Üres lista esetén 1 a visszatérési érték.
-'''
-
-
-
-#--------------------------
-'''
-    A benne_van_a_stringben nevű függvény.
-    első paraméterként egy stringet kap,
-    második paraméterként egy betüt.
-    Visszatérési értéke True, ha  a betü benne van a stringben.
-    A visszatérési érték False, ha  a betü nics benne a stringben.
-'''
-
-
-
-#--------------------------
-''' 
-    A negativok_szama nevű függvény,
-    paraméterként egy számokat tartalmazó listát kap és
-    visszatér egy számokat tartalmazó lista negativ számainak számával.
+    visszatér egy számokat tartalmazó lista pozitív számainak számával.
     
     Üres lista esetén 0 a visszatérési érték.
 '''
 
-
-
-#--------------------------
-'''
-    A negativok_kivalogatasa nevű függvény,
-    visszatér egy listával, 
-      amely a paraméterként átadott számokat tartalmazó lista
-      negatív számait tartalmazza.
-'''
-
-
-
-#--------------------------
-''' 
-    A lista_atlag nevű függvény,
-    paraméterként egy számokat tartalmazó listát kap és
-    visszatér a lista számainak átlagával.
-'''
-
-
-
-#--------------------------
-''' 
-    A legkisebb nevű függvény,
-    paraméterként egy számokat tartalmazó listát kap és
-    visszatér a lista legkisebb számával.
-    Üres lista esetén None a visszatérési érték.
-    
-    A feladat megoldása során nem használhatod a min() függvényt!
-'''
 
 
 
@@ -129,42 +23,31 @@ def kereses_a_listaban(lista, szam):
 
 #--------------------------
 '''
-    A kereses_a_stringben nevű függvény,
-    első paraméterként egy sztringet kap,
-    második paraméterként egy betüt.
-    Visszatérési érték a paraméterként megadott betü
-        első előfordulási helye a stringben. 
+    A kereses_a_listaban nevű függvény.
+    Első bemeneti paraméter egy lista,
+    második bemeneti paraméter egy szám.
+    A visszatérési érték a paraméterként megadott szám
+        első előfordulási helye a listában.
     
-    A visszatérési érték None, ha a betü nics benne a stringben.
+    A visszatérési érték None, ha a szám nics benne a listában.
 '''
 
 
 
 #--------------------------
-'''
-    A faktorialis nevű függvény,
-    visszatér a paraméterként megkapott szám faktoriálisával.
-'''
-
-def faktorialis(a):
-    osszeg = 1
-    for x in range(1, a + 1):
-        osszeg *= x
+''' 
+    Az elso_karakter nevű függvény,
+    paraméterként egy stringet kap és
+    visszatér a string első karakterével.
     
-    return osszeg
-
-
-#--------------------------
+    Üres string esetén None a visszatérési érték.
 '''
-    A benne_van_a_listaban nevű függvény,
-    első paraméterként egy listát kap,
-    második paraméterként egy számot.
-    A visszatérési érték True, ha  a szám benne van a listában.
-    A visszatérési érték False, ha  a szám nics benne a listában.
+def elso_karakter(szoveg):
+    if(szoveg == ""):
+        return None
+
+    return szoveg[0]
     
-    Üres lista esetén a visszatérési érték False.
-'''
-
 
 
 #--------------------------
@@ -180,43 +63,68 @@ def faktorialis(a):
 
 #--------------------------
 ''' 
-    A parosok_szama  nevű függvény,
-    visszatér egy számokat tartalmazó lista páros számainak számával.
-    
-    Üres lista esetén 0 a visszatérési érték.
-'''
-
-def parosok_szama(a):
-    p = 0
-    if(a == []):
-        return 0
-    
-    for szam in a:
-        if(szam % 2 == 0):
-            p += 1
-    
-    return p
-
-
-
-
-#--------------------------
-'''
-    A paratlanok_kivalogatasa nevű függvény,
+    A pozitivok_kivalogatasa nevű függvény,
     visszatér egy listával, 
         amely a paraméterként átadott számokat tartalmazó lista
-        páratlan számait tartalmazza.
+        pozitiv számait tartalmazza. 
 '''
 
 
 
 #--------------------------
 ''' 
-    A pozitivok_szama nevű függvény,
+    Az osszeg  nevű függvény,
     paraméterként egy számokat tartalmazó listát kap és
-    visszatér egy számokat tartalmazó lista pozitív számainak számával.
+    visszatér a lista számainak összegével.
+    Üres lista esetén 0 a visszatérési érték.
+    A feladat megoldása során nem használhatod a sum() függvényt!
+'''
+def osszeg(lista):
+    if(lista == []):
+        return 0
+
+    osszeg = 0
+
+    for szam in lista:
+        osszeg += szam
+
+    return osszeg
+
+
+#--------------------------
+''' 
+    A lista_atlag nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista számainak átlagával.
+'''
+
+def lista_atlag(lista):
+    if(lista == []):
+        return 0
+
+    osszeg = 0
+    for szam in lista:
+        osszeg += szam
+
+    return osszeg / len(lista)
+
+
+#--------------------------
+''' 
+    A parosok_szama  nevű függvény,
+    visszatér egy számokat tartalmazó lista páros számainak számával.
     
     Üres lista esetén 0 a visszatérési érték.
+'''
+
+
+
+#--------------------------
+'''
+    A parosok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+        amely a paraméterként átadott számokat tartalmazó lista
+        páros számait tartalmazza.
 '''
 
 
@@ -235,10 +143,112 @@ def parosok_szama(a):
 
 #--------------------------
 '''
-    A parosok_kivalogatasa nevű függvény,
+    A paratlanok_kivalogatasa nevű függvény,
     visszatér egy listával, 
         amely a paraméterként átadott számokat tartalmazó lista
-        páros számait tartalmazza.
+        páratlan számait tartalmazza.
+'''
+
+
+
+#--------------------------
+'''
+    A benne_van_a_stringben nevű függvény.
+    első paraméterként egy stringet kap,
+    második paraméterként egy betüt.
+    Visszatérési értéke True, ha  a betü benne van a stringben.
+    A visszatérési érték False, ha  a betü nics benne a stringben.
+'''
+
+def benne_van_a_stringben(sztring, betu):
+    if betu in sztring:
+        return True
+    else:
+        return False
+
+
+#--------------------------
+''' 
+    A szorzat  nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista számainak szorzatával.
+    
+    Üres lista esetén 1 a visszatérési érték.
+'''
+
+
+
+#--------------------------
+''' 
+    A negativok_szama nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér egy számokat tartalmazó lista negativ számainak számával.
+    
+    Üres lista esetén 0 a visszatérési érték.
+'''
+
+
+
+#--------------------------
+'''
+    A faktorialis nevű függvény,
+    visszatér a paraméterként megkapott szám faktoriálisával.
+'''
+
+
+
+#--------------------------
+'''
+    A benne_van_a_listaban nevű függvény,
+    első paraméterként egy listát kap,
+    második paraméterként egy számot.
+    A visszatérési érték True, ha  a szám benne van a listában.
+    A visszatérési érték False, ha  a szám nics benne a listában.
+    
+    Üres lista esetén a visszatérési érték False.
+'''
+
+
+
+#--------------------------
+''' 
+    A legkisebb nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista legkisebb számával.
+    Üres lista esetén None a visszatérési érték.
+    
+    A feladat megoldása során nem használhatod a min() függvényt!
+'''
+
+
+
+#--------------------------
+'''
+    A negativok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+      amely a paraméterként átadott számokat tartalmazó lista
+      negatív számait tartalmazza.
+'''
+
+def negativok_kivalogatasa(lista):
+    neg = []
+
+    for szam in lista:
+        if(szam < 0):
+            neg.append(szam)
+
+    return neg
+
+
+#--------------------------
+'''
+    A kereses_a_stringben nevű függvény,
+    első paraméterként egy sztringet kap,
+    második paraméterként egy betüt.
+    Visszatérési érték a paraméterként megadott betü
+        első előfordulási helye a stringben. 
+    
+    A visszatérési érték None, ha a betü nics benne a stringben.
 '''
 
 
